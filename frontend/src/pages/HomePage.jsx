1ab3ba97bee9ec25847fc734224a4ce15cb96628
Vidarshana11 <vidarshanajayasena92@gmail.com>
2025-09-19 03:07:42 +0530

    Commit second

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -80,7 +80,7 @@ const HomePage = () => {
             <Link to="/marketplace" className="bg-primary-500 hover:bg-primary-600 text-white rounded-md h-10 px-6 flex items-center justify-center text-center">
               <ShoppingCart className="inline w-5 h-5 mr-2" />
               Browse Products
-            </Link>
+            </Link>         
             <Link to="/profile" className="border-2 border-primary-500 text-primary-500 hover:bg-primary-500 hover:text-white rounded-md h-10 px-6 flex items-center justify-center text-center">
               View Profile
             </Link>
